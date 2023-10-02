de9d12a33b5add892c563d7ba75f33a3743abe23
KantaHasegawa <gyoza20000@gmail.com>
2023-10-02 23:38:21 +0900

    setInterval→DOMContentLoaded

--- popup.js
+++ popup.js
@@ -18,9 +18,15 @@ chrome.runtime.onMessage.addListener((message) => {
     }
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById('capture')
+    button.textContent = "START";
+    button.addEventListener('click', () => {
+        chrome.runtime.sendMessage({action: "capture"}).then((response) => {
+            console.log(response)
+        })
+    })
 
-setTimeout(async () => {
-    console.log('setup ok')
     document.getElementById('clickAtCoordinates').addEventListener('click', function() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             const currentTab = tabs[0];
@@ -31,19 +37,7 @@ setTimeout(async () => {
             });
         });
     });
-}, 5000);
-
-setTimeout(async() => {
-    const captureButton = document.getElementById('capture');
-    captureButton.addEventListener('click', () => {
-        chrome.runtime.sendMessage({action: "capture"}).then((response) => {
-            console.log(response)
-        }
-    )
-    }
-)
-}, 5000)
-    
+})
 
 function simulateClickOnCanvas(x, y) {
     const canvas = document.querySelector('canvas');  // 最初のcanvas要素を取得します。
